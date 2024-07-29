4be41188091b94aa76272f37b0008f002ae2545c
gadmarkovits <gad.markovits@intel.com>
2024-07-29 06:34:14 -0700

    opea generator wip
    Signed-off-by: gadmarkovits <gad.markovits@intel.com>

---
+++ integrations/opea/src/haystack_integrations/components/generators/opea/backend.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Optional, Tuple
+
+
+class GeneratorBackend(ABC):
+    def __init__(self, model_kwargs: Optional[Dict[str, Any]] = None):
+        """
+        Initialize the backend.
+
+        :param model_kwargs:
+            Additional keyword arguments to pass to the model.
+        """
+        self.model_kwargs = model_kwargs or {}
+
+    @abstractmethod
+    def generate(self, prompt: str) -> Tuple[List[str], List[Dict[str, Any]]]:
+        """
+        Invoke the backend and prompt the model.
+
+        :param prompt:
+            Prompt text.
+        :return:
+            Vector representation of the generated texts related
+            metadata returned by the service.
+        """
+        pass
